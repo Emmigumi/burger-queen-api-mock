5d5990d29b6eb2d2da4eed66f86e5f96d5c0a90c
Jammie Hurtado <98611521+Emmigumi@users.noreply.github.com>
2022-07-26 20:21:14 -0500

    uncoment line 20-server

--- server.js
+++ server.js
@@ -17,7 +17,7 @@ app.db = router.db
 
 // You must apply the auth middleware before the router
 app.use(middlewares);
-/* app.use(rules); */
+app.use(rules);
 app.use(auth);
 app.use(router);
 app.listen(port, () => {
